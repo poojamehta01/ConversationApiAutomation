905e246ae2cc69c0ff7d8e114a1e779d32170fb0
poojamehta <poojamehta1197@gmail.com>
2023-03-21 08:30:02 +0530

    Add dataProvider

--- src/main/java/com/api/automation/helpers/ConversationApiDataProviderHelper.java
+++ src/main/java/com/api/automation/helpers/ConversationApiDataProviderHelper.java
@@ -1,2 +1,20 @@
-package com.api.automation.helpers;public class ConversationApiDataProviderHelper {
+package com.api.automation.helpers;
+
+import java.lang.reflect.Method;
+import org.testng.annotations.DataProvider;
+
+public class ConversationApiDataProviderHelper {
+
+  @DataProvider(name = "conversationApi_dataProvider")
+  public static Object[][] conversationApi_dataProvider(Method m) {
+
+    switch (m.getName()) {
+      case "createConversation":
+        return new Object[][]{
+
+        };
+      default:
+        return new Object[][] {};
+    }
+  }
 }
