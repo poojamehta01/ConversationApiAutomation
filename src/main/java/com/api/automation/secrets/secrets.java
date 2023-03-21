929c6141a9c57f6f48339321b17ae682a7a3e8ad
poojamehta <poojamehta1197@gmail.com>
2023-03-21 07:56:36 +0530

    Add secrets

--- src/main/java/com/api/automation/secrets/secrets.java
+++ src/main/java/com/api/automation/secrets/secrets.java
@@ -1,2 +1,14 @@
-package com.api.automation.secrets;public class secrets {
+package com.api.automation.secrets;
+
+import static com.api.automation.constants.CommonConstants.SECRETS_FILE_NAME;
+
+import com.api.automation.helpers.CommonTestHelper;
+
+public class secrets {
+
+  public static final String CLIENT_ID =
+      CommonTestHelper.getSecretValues("acClientId", SECRETS_FILE_NAME, "");
+  public static final String CLIENT_SECRET =
+      CommonTestHelper.getSecretValues("acClientSecret", SECRETS_FILE_NAME, "");
+
 }
